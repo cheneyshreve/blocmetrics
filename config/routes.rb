e175abe734ceb995c738f2def962be4eddf3e8c4
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-03-22 05:36:03 -0700

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  
+
   get 'welcome/index'
 
   get 'welcome/about'
 
-  root to: 'welcome#index'
+  root 'welcome#index'
 
 end
