c5298fbb656214a484216e863299fd0379c17ccd
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-03-22 05:41:28 -0700

    updated prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,19 +86,31 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_options = { from: "shedevsnow@gmail.com" }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.active_record.dump_schema_after_migration = true
+    config.action_mailer.default_options = { from: "shedevsnow@gmail.com" }
 
-  config.action_mailer.smtp_settings = {
+    config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD'],
-  }
+    }
+
+    # ActionMailer Config
+    config.action_mailer.default_url_options = { :host => 'https://stark-forest-69447.herokuapp.com'}
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = false
+
+    if ENV["RAILS_LOG_TO_STDOUT"].present?
+      logger = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger = ActiveSupport::TaggedLogging.new(logger)
+    end
+
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
