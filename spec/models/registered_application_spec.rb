064492dbca56e20c0bf546fb6d5d82e4ca5141d5
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-03-22 11:43:46 -0700

    updated user association, rspecs

--- spec/models/registered_application_spec.rb
+++ spec/models/registered_application_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe RegisteredApplication, type: :model do
   let (:my_user) {User.create!(email: 'blochead@bloc.io', password: 'password', password_confirmation: 'password')}
   let(:registered_application) { RegisteredApplication.create!(user_id: my_user.id, name: "application name", url: "http://www.test.url.com" )}
 
+   it { is_expected.to belong_to(:user) }
+
   describe "attributes" do
     it "has a name" do
-      expect(registered_application).to have_attributes(name: registered_application.name )
+      expect(registered_application).to have_attributes(name: registered_application.name)
     end
     it "has a url" do
       expect(registered_application).to have_attributes(url: registered_application.url)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+
+  let (:user) {User.create!(email: "test@email.com", password: "password", password_confirmation: "password")}
+
+  let(:registered_application) { RegisteredApplication.create!(user_id: user.id, name: "application name", url: "http://www.test.url2.com" )}
+
+  it { is_expected.to have_many(:registered_applications) }
+
+  describe "attributes" do
+     it "responds to email" do
+      expect(user).to respond_to(:email)
+     end
+
+     it "responds to password" do
+       expect(user).to respond_to(:password)
+     end
+
+     it "responds to password_confirmation" do
+       expect(user).to respond_to(:password_confirmation)
+     end
+  end
+
 end
