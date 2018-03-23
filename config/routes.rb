da44a1c822403b5b32219b793938af2be55cb5c9
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-03-23 11:09:26 -0700

    updated user controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "registered_applications/show"
   get "registered_applications/new"
   get "registered_applications/edit"
+  get "users/show"
 
   devise_for :users
   resources :users
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,19 @@ require 'rails_helper'
 require 'devise'
 
 RSpec.describe UsersController, type: :controller do
+   let (:test_user) {User.create!(email: "tester@test.com", password: "password", password_confirmation: "password" ) }
 
+  describe "GET #show" do
+    before(:each) do
+       @request.env["devise.mapping"] = Devise.mappings[:user]
+       user = FactoryBot.create(:user)
+       sign_in user
+    end
 
+    it "returns http success" do
+      get :show, params: { id: test_user.id }
+      expect(response).to have_http_status(:success)
+    end
+  end
 
 end
