8e19b31e9857f9eb7f8cb04baea9551781764f43
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-03-24 13:20:28 -0700

    updates to prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,11 +89,8 @@ Rails.application.configure do
     # ActionMailer Config
     config.action_mailer.default_url_options = { :host => 'https://stark-forest-69447.herokuapp.com'}
 
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-
-    config.action_mailer.smtp_settings = {
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
       address:              'smtp.sendgrid.net',
       port:                 587,
       domain:               'heroku.com',
@@ -102,7 +99,6 @@ Rails.application.configure do
       authentication:       'plain',
       enable_starttls_auto: true
     }
-
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
