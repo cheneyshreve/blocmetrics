065b5c30c56ee214e9a73a83f375a59a30056aa7
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-03-24 12:46:04 -0700

    still working on prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,8 +93,7 @@ Rails.application.configure do
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
 
-    ActionMailer::Base.delivery_method = :smtp
-    ActionMailer::Base.smtp_settings = {
+    config.action_mailer.smtp_settings = {
       address:              'smtp.sendgrid.net',
       port:                 587,
       domain:               'heroku.com',
