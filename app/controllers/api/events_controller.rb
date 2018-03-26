4088922bd98bab3f6aae67d3b9bd791391b63ea7
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-03-26 14:11:42 -0700

    working on debugging

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,7 +1,6 @@
 class API::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
-
-  before_filter :set_access_control_headers
+  before_action :set_access_control_headers
 
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
